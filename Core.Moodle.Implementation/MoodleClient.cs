a24f98e0f0020ffc69e887be7d7563d4e975b922
Klausmd5 <klaus.scheiboeck@vortexdata.net>
2022-09-26 09:55:12 +0200

    add Moodle-interfaces

--- Core.Moodle.Implementation/MoodleClient.cs
+++ Core.Moodle.Implementation/MoodleClient.cs
@@ -4,4 +4,18 @@ namespace Core.Moodle.Implementation;
 
 public class MoodleClient : IMoodleClient
 {
+    public Task<MoodleAssignment> GetAssignment(string token)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<MoodleCourse> GetCourses(string token)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<MoodleAssignment> GetAssignment(string token)
+    {
+        throw new NotImplementedException();
+    }
 }
