a21f622c21eba830f751b64eea0572a1a6ccb180
EinboeckFranz <EinboeckF180011@sus.htl-grieskirchen.at>
2022-09-26 16:57:33 +0200

    contacts list nullable - 4real this time

--- Core.Moodle.Interface/DetailedMoodleCourse.cs
+++ Core.Moodle.Interface/DetailedMoodleCourse.cs
@@ -2,7 +2,7 @@ namespace Core.Moodle.Interface;
 
 public class DetailedMoodleCourse : MoodleCourse
 {
-    public List<string>? Contacts { get; set; }
+    public List<string?>? Contacts { get; set; }
     public string? Description { get; set; }
     public string? CategoryName { get; set; }
     public DateTime? StartDate { get; set; }
