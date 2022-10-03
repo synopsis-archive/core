7c2acfef1c293fa1355519a0a5f9aa1c949f181d
Andreas Aigner <aigner.andi05@gmail.com>
2022-10-03 10:34:58 +0200

    Create plugin interface project

---
+++ Core.Plugin.Interface/ICorePlugin.cs
@@ -0,0 +1,16 @@
+namespace Core.Plugin.Interface;
+
+public interface ICorePlugin
+{
+    /*
+     * Use this method to register your classes for dependency injection.
+     * Run methods of the backend builder object here.
+     */
+    public void ConfigureServices(WebApplicationBuilder builder);
+
+    /*
+     * Use this to register your controllers.
+     * You can configure the app's request handling pipeline and their middleware components here.
+     */
+    public void Configure(WebApplication app);
+}
