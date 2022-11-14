418e1308600dd491b13d0d4157cce64ef3c7ea61
EliasAspoeck <elias.aspoeck@gmail.com>
2022-11-14 09:23:32 +0100

    remove unnecessary file HomeworkRequest.cs

--- Core.WebUntis.Implementation/RequestTypes/HomeworkRequest.cs
+++
@@ -1,9 +0,0 @@
-using System.Text.Json.Serialization;
-
-namespace Core.WebUntis.Implementation.RequestTypes;
-
-public class HomeworkRequest
-{
-    [JsonPropertyName("startDate")] public int StartDate { get; set; }
-    [JsonPropertyName("endDate")] public int EndDate { get; set; }
-}
