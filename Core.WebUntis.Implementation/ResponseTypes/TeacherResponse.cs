fc646c9b1a11b04d3f2afcd10ceb3bb10d16eda1
Simon Feichtlbauer <simon.feichtlbauer@gmail.com>
2022-11-14 09:42:53 +0100

    added TeacherResponse

---
+++ Core.WebUntis.Implementation/ResponseTypes/TeacherResponse.cs
@@ -0,0 +1,27 @@
+﻿using System.Runtime.Versioning;
+using System.Text.Json.Serialization;
+
+namespace Core.WebUntis.Implementation.ResponseTypes;
+
+public class TeacherResponse
+{
+    [JsonPropertyName("id")] public int Id { get; set; }
+    [JsonPropertyName("lastName")] public string LastName { get; set; }
+    [JsonPropertyName("firstName")] public string FirstName { get; set; }
+    [JsonPropertyName("abbreviation")] public string Abbreviation { get; set; }
+    [JsonPropertyName("foreColor")] public string ForeColor { get; set; }
+    [JsonPropertyName("backColor")] public string BackColor { get; set; }
+
+    public Teacher Convert()
+    {
+        return new Teacher
+        {
+            Id = Id,
+            Abbreviation = Abbreviation,
+            BackColor = Abbreviation,
+            FirstName = FirstName,
+            ForeColor = ForeColor,
+            LastName = LastName
+        };
+    }
+}
