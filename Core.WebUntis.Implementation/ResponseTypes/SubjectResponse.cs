036c14cd261fd7fc1b37d9be42aa1a3266b9374a
Simon Feichtlbauer <simon.feichtlbauer@gmail.com>
2022-11-14 09:18:38 +0100

    created SubjectResponse

---
+++ Core.WebUntis.Implementation/ResponseTypes/SubjectResponse.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Core.WebUntis.Implementation.ResponseTypes;
+
+public class SubjectResponse
+{
+    [JsonPropertyName("id")] public int Id { get; set; }
+    [JsonPropertyName("name")] public string Name { get; set; }
+    [JsonPropertyName("longName")] public string LongName { get; set; }
+    [JsonPropertyName("foreColorHex")] public string ForeColorHex { get; set; }
+    [JsonPropertyName("backColorHex")] public string BackColorHex { get; set; }
+
+    public Subject Convert(SubjectResponse response)
+    {
+        return new Subject
+        {
+            Id = response.Id,
+            Name = response.Name,
+            LongName = response.LongName,
+            ForeColorHex = response.ForeColorHex,
+            BackColorHex = response.BackColorHex
+        };
+    }
+}
