5dc66bdf3be5e40a2de2504081c1c94aaabc833c
Simon Feichtlbauer <simon.feichtlbauer@gmail.com>
2022-11-14 09:24:43 +0100

    added HolidayResponse

---
+++ Core.WebUntis.Implementation/ResponseTypes/HolidayResponse.cs
@@ -0,0 +1,57 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Core.WebUntis.Implementation.ResponseTypes;
+
+public class HolidayResponse
+{
+    [JsonPropertyName("id")] public int Id { get; set; }
+    [JsonPropertyName("name")] public string Name { get; set; }
+    [JsonPropertyName("longName")] public string LongName { get; set; }
+
+    private int startDateUntis;
+    [JsonPropertyName("startDate")]
+    public int StartDateUntis
+    {
+        get
+        {
+            return this.startDateUntis;
+        }
+        set
+        {
+            this.startDateUntis = value;
+            StartDate = UntisDateTimeMethods.ConvertUntisDateToDate(value);
+        }
+    }
+
+    private int endDateUntis;
+
+    [JsonPropertyName("endDate")]
+    public int EndDateUntis
+    {
+        get
+        {
+            return this.endDateUntis;
+        }
+        set
+        {
+            this.endDateUntis = value;
+            EndDate = UntisDateTimeMethods.ConvertUntisDateToDate(value);
+        }
+    }
+
+    public DateTime StartDate { get; set; }
+    public DateTime EndDate { get; set; }
+
+
+    public Holiday Convert()
+    {
+        return new Holiday
+        {
+            Id = Id,
+            Name = Name,
+            EndDate = EndDate,
+            LongName = LongName,
+            StartDate = StartDate
+        };
+    }
+}
