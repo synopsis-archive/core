eb99659de1811090045045eb4d0ad2e1b86d84ec
Simon Feichtlbauer <simon.feichtlbauer@gmail.com>
2022-11-14 09:55:39 +0100

    added TimetableRequest and TimetableResponse

---
+++ Core.WebUntis.Implementation/RequestTypes/TimetableRequest.cs
@@ -0,0 +1,11 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Core.WebUntis.Implementation.RequestTypes;
+
+public class TimetableRequest
+{
+    [JsonPropertyName("type")] public int Type { get; set; }
+    [JsonPropertyName("id")] public int? Id { get; set; }
+    [JsonPropertyName("startDate")] public int StartDate { get; set; }
+    [JsonPropertyName("endDate")] public int EndDate { get; set; }
+}
---
+++ Core.WebUntis.Implementation/ResponseTypes/TimetableResponse.cs
@@ -0,0 +1,36 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Core.WebUntis.Implementation.ResponseTypes;
+
+public class TimetableResponse
+{
+    [JsonPropertyName("start")] public DateTime Start { get; set; }
+    [JsonPropertyName("end")] public DateTime End { get; set; }
+    [JsonPropertyName("id")] public int Id { get; set; }
+    [JsonPropertyName("classIds")] public List<int> ClassIds { get; set; }
+    [JsonPropertyName("roomIds")] public List<int> RoomIds { get; set; }
+    [JsonPropertyName("subjectIds")] public List<int> SubjectIds { get; set; }
+    [JsonPropertyName("teacherIds")] public List<int> TeacherIds { get; set; }
+    [JsonPropertyName("lessonType")] public string LessonType { get; set; }
+    [JsonPropertyName("text")] public string Text { get; internal set; }
+    [JsonPropertyName("statisticalFlags")] public string StatisticalFlags { get; set; }
+    [JsonPropertyName("code")] public string Code { get; internal set; }
+
+    public Period Convert()
+    {
+        return new Period
+        {
+            Id = Id,
+            Code = Enum.Parse<Code>(Code),
+            Start = Start,
+            End = End,
+            Text = Text,
+            ClassIds = ClassIds,
+            LessonType = Enum.Parse<LessonType>(LessonType),
+            RoomIds = RoomIds,
+            StatisticalFlags = StatisticalFlags,
+            SubjectIds = SubjectIds,
+            TeacherIds = TeacherIds
+        };
+    }
+}
