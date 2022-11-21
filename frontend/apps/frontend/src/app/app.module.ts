9507758d837cd2521854decd55b7724c89bfecab
annalena-kagerer <annalena.kagerer@outlook.com>
2022-11-21 08:45:52 +0100

    Add Buttons

--- frontend/apps/frontend/src/app/app.module.ts
+++ frontend/apps/frontend/src/app/app.module.ts
@@ -1,19 +1,14 @@
-import {NgModule} from "@angular/core";
-import {BrowserModule} from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
 
-import {AppComponent} from "./app.component";
-import {MainframeConnectorModule} from "mainframe-connector";
+import { AppComponent } from "./app.component";
+import { MainframeConnectorModule } from "mainframe-connector";
+import {SharedModule} from "./shared/shared.module";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    MainframeConnectorModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, MainframeConnectorModule, SharedModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule {
-}
+export class AppModule {}
