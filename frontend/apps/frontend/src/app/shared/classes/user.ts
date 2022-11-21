f02767a8fef88a686b820aeebcff30d4088bbb9a
annalena-kagerer <annalena.kagerer@outlook.com>
2022-11-21 10:07:35 +0100

    Add description and some changes

--- frontend/apps/frontend/src/app/shared/classes/user.ts
+++ frontend/apps/frontend/src/app/shared/classes/user.ts
@@ -1,7 +1,10 @@
 ﻿export class User {
   constructor(
+    // name of user
     public username: string,
+    // clazz of user - example: 5B
     public clazz: string,
+    // role of user
     public role: string,
   ) {
   }
