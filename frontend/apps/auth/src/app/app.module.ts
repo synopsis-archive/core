d41c87aa650b78e3f039ca5319f220a1481af0ef
Michael Ungar <michael.ungar03@gmail.com>
2022-11-27 22:17:36 +0100

    feat: add onboarding module

--- frontend/apps/auth/src/app/app.module.ts
+++ frontend/apps/auth/src/app/app.module.ts
@@ -2,9 +2,9 @@ import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 
 import { AppComponent } from "./app.component";
-import {RouterModule} from "@angular/router";
+import { RouterModule } from "@angular/router";
 import { appRoutes } from "./app.routes";
-import {LoginModule} from "./login/login.module";
+import { LoginModule } from "./login/login.module";
 
 @NgModule({
   declarations: [AppComponent],
---
+++ frontend/apps/auth/src/app/onboarding/onboarding.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+
+import { OnboardingRoutingModule } from "./onboarding-routing.module";
+import { OnboardingComponent } from "./onboarding.component";
+
+@NgModule({
+  declarations: [OnboardingComponent],
+  imports: [CommonModule, OnboardingRoutingModule],
+  bootstrap: [OnboardingComponent],
+})
+export class OnboardingModule {}
