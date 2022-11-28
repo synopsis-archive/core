dbb483d6d7da3c6834fb0d6ff24ece2abc844b03
littlegamer <jneissl27@gmail.com>
2022-11-28 18:03:14 +0100

    Document IDTokenPayload, deserialize connectedPlatforms upon decoding jwt

--- src/lib/mainframe-id-token.service.ts
+++ src/lib/mainframe-id-token.service.ts
@@ -10,23 +10,44 @@ export class MainframeIdTokenService {
   }
 
   public getJwt(): string {
-    return "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0eXBlIjoic3R1ZGVudCIsInVzZXJuYW1lIjoic2ZlaWNodGxiYXVlcjE4IiwidXVpZCI6InN1cGEgdXVpZCBob3N0IGR1IGRvIiwicm9sbGUiOiJpbW1hIG51IHN0dWRlbnQiLCJlbWFpbCI6InNmZWljaHRsYmF1ZXIxOEBzdXMuaHRsLWdyaWVza2lyY2hlbi5hdCIsImNvbm5lY3RlZFBsYXRmb3JtcyI6IntcInBsYXRmb3JtTmFtZVwiOiBcImZlaWNodGxiYXVlci5kZG5zLm5ldFwiLCBcInBsYXRmb3JtVXJsXCI6XCJodHRwOi8vZmVpY2h0bGJhdWVyLmRkbnMubmV0L1wifSIsIm1hdHJpa2VsbnVtbWVyIjoiNjkiLCJrbGFzc2UiOiI1QiJ9.Mrvk7V0ZKjugA_r-uU6qQhv3zHzLs3M3AXtKmRqR5Ro";
+    return "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0eXBlIjoiaWQtdG9rZW4iLCJ1c2VybmFtZSI6InNmZWljaHRsYmF1ZXIxOCIsInV1aWQiOiJzdXBhIHV1aWQgaG9zdCBkdSBkbyIsInJvbGxlIjoic3R1ZGVudCIsImVtYWlsIjoic2ZlaWNodGxiYXVlcjE4QHN1cy5odGwtZ3JpZXNraXJjaGVuLmF0IiwiY29ubmVjdGVkUGxhdGZvcm1zIjoiW3tcIlBsYXRmb3JtTmFtZVwiOlwiZmVpY2h0bGJhdWVyLmRkbnMubmV0XCIsXCJVc2VybmFtZVwiOlwic2ZlaWNodGxiYXVlcjE4XCJ9LHtcIlBsYXRmb3JtTmFtZVwiOlwic3BpZWxhZmZlLmRlXCIsXCJVc2VybmFtZVwiOlwiY29vbGVyc2ltb242OVwifSx7XCJQbGF0Zm9ybU5hbWVcIjpcInNjaG51cGZ0YWJha2JpbGxpZ2VyMjQuZGVcIixcIlVzZXJuYW1lXCI6XCJzZWljaHRsYmF1ZXJmaW1vblwifV0iLCJtYXRyaWtlbG51bW1lciI6IjY5Iiwia2xhc3NlIjoiNUIifQ.2-Ee5rHvmjjcMYn-3YA4nvfo9kGOX6utlX8_lA961os";
   }
 
   public decodeJwt(token: string): IDTokenPayload {
-    return jwtDecode<IDTokenPayload>(token);
+    const payload = jwtDecode<IDTokenPayload>(token);
+    payload.connectedPlatformsDeserialized = JSON.parse(payload.connectedPlatforms);
+    return payload;
   }
 
 }
 
 // modelled after the Claims Property of class IDToken in the Core.Backend.Secure project
 export interface IDTokenPayload {
+  // type: "id-token"
   type: string,
+
+  // username: ldap-username
   username: string,
+
+  // uid: db-uuid
   uuid: string,
+
+  // rolle: <schüler, lehrer, staff>
   rolle: string,
+
+  // Email
   email: string,
+
+  // connectedPlatforms: serialisiertes Json-Array -- Plattformen mit hinterlegten credentials
   connectedPlatforms: string,
+
+  // FIXME: i hob keine Ahnung, wie des moi ausschaun soid
+  // connectedPlatformsDeserialized: connectedPlatforms, oba deserialisiert
+  connectedPlatformsDeserialized: { PlatformName: string, Username: string }[],
+
+  // matrikelnummer - nur bei Schülern verfügbar
   matrikelnummer: string | undefined,
+
+  // klasse - nur bei Schülern verfügbar
   klasse: string | undefined,
 }
