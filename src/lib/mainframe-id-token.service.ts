37afa29016127830d71e4223807df1f711d12b66
littlegamer <jneissl27@gmail.com>
2022-11-28 18:08:43 +0100

    Update IDTokenPayload

--- src/lib/mainframe-id-token.service.ts
+++ src/lib/mainframe-id-token.service.ts
@@ -10,7 +10,7 @@ export class MainframeIdTokenService {
   }
 
   public getJwt(): string {
-    return "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0eXBlIjoiaWQtdG9rZW4iLCJ1c2VybmFtZSI6InNmZWljaHRsYmF1ZXIxOCIsInV1aWQiOiJzdXBhIHV1aWQgaG9zdCBkdSBkbyIsInJvbGxlIjoic3R1ZGVudCIsImVtYWlsIjoic2ZlaWNodGxiYXVlcjE4QHN1cy5odGwtZ3JpZXNraXJjaGVuLmF0IiwiY29ubmVjdGVkUGxhdGZvcm1zIjoiW3tcIlBsYXRmb3JtTmFtZVwiOlwiZmVpY2h0bGJhdWVyLmRkbnMubmV0XCIsXCJVc2VybmFtZVwiOlwic2ZlaWNodGxiYXVlcjE4XCJ9LHtcIlBsYXRmb3JtTmFtZVwiOlwic3BpZWxhZmZlLmRlXCIsXCJVc2VybmFtZVwiOlwiY29vbGVyc2ltb242OVwifSx7XCJQbGF0Zm9ybU5hbWVcIjpcInNjaG51cGZ0YWJha2JpbGxpZ2VyMjQuZGVcIixcIlVzZXJuYW1lXCI6XCJzZWljaHRsYmF1ZXJmaW1vblwifV0iLCJtYXRyaWtlbG51bW1lciI6IjY5Iiwia2xhc3NlIjoiNUIifQ.2-Ee5rHvmjjcMYn-3YA4nvfo9kGOX6utlX8_lA961os";
+    return "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0eXBlIjoiaWQtdG9rZW4iLCJ1c2VybmFtZSI6InNmZWljaHRsYmF1ZXIxOCIsInV1aWQiOiJzdXBhIHV1aWQgaG9zdCBkdSBkbyIsInJvbGxlIjoic3R1ZGVudCIsImVtYWlsIjoic2ZlaWNodGxiYXVlcjE4QHN1cy5odGwtZ3JpZXNraXJjaGVuLmF0IiwiY29ubmVjdGVkUGxhdGZvcm1zIjoiW1wiZmVpY2h0bGJhdWVyLmRkbnMubmV0XCIsXCJzcGllbGFmZmUuZGVcIixcInNjaG51cGZ0YWJha2JpbGxpZ2VyMjQuYXRcIl0iLCJtYXRyaWtlbG51bW1lciI6IjY5Iiwia2xhc3NlIjoiNUIifQ._VJ_BC8x6GTTMbQ_ZBKD84UArUWdaSmGOTKj8SghEFM";
   }
 
   public decodeJwt(token: string): IDTokenPayload {
@@ -41,9 +41,8 @@ export interface IDTokenPayload {
   // connectedPlatforms: serialisiertes Json-Array -- Plattformen mit hinterlegten credentials
   connectedPlatforms: string,
 
-  // FIXME: i hob keine Ahnung, wie des moi ausschaun soid
   // connectedPlatformsDeserialized: connectedPlatforms, oba deserialisiert
-  connectedPlatformsDeserialized: { PlatformName: string, Username: string }[],
+  connectedPlatformsDeserialized: string[],
 
   // matrikelnummer - nur bei Schülern verfügbar
   matrikelnummer: string | undefined,
