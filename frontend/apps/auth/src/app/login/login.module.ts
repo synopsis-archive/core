5602bb0a1ab49c95164d0bca22dce0c5b50c3349
littlegamer757 <jneissl27@gmail.com>
2022-11-28 09:24:39 +0100

    Remove redundant component
    Reviewed-by: littlegamer757 <jneissl27@gmail.com>

--- frontend/apps/auth/src/app/login/login.module.ts
+++ frontend/apps/auth/src/app/login/login.module.ts
@@ -2,13 +2,12 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 
 import { LoginRoutingModule } from "./login-routing.module";
-import { CredentialPageComponent } from "./credential-page/credential-page.component";
 import { DailyQuoteComponent } from "./daily-quote/daily-quote.component";
 import { LoginComponent } from "./login.component";
 import { SharedModule } from "../shared/shared.module";
 
 @NgModule({
-  declarations: [CredentialPageComponent, DailyQuoteComponent, LoginComponent],
+  declarations: [DailyQuoteComponent, LoginComponent],
   imports: [CommonModule, SharedModule, LoginRoutingModule],
   bootstrap: [LoginComponent],
 })
