8d49560a6249d2cc756f8f2618c1830fa219d11c
Lea <seifried.lea@gmail.com>
2022-12-12 11:38:43 +0100

    Create home-page categories and plugins

--- frontend/apps/frontend/src/app/home/home/home.component.ts
+++ frontend/apps/frontend/src/app/home/home/home.component.ts
@@ -3,6 +3,7 @@ import {
   IDTokenPayload,
   MainframeIdTokenService
 } from "../../../../../../libs/mainframe-connector/src/lib/mainframe-id-token.service";
+import {Plugin} from "../../shared/classes/plugin";
 
 @Component({
   selector: "app-home",
@@ -15,7 +16,22 @@ export class HomeComponent implements OnInit {
 
   jwtPayload: IDTokenPayload | undefined;
 
+  getNewCategory = (name: string, icon: string) => new Category( name, `../../../assets/icons/${icon}`);
+
+  courses: Category[] = [
+    this.getNewCategory('Favoriten', 'star.svg'),
+    this.getNewCategory('Meine', 'user-search.svg'),
+    this.getNewCategory('Bald fällig', 'hourglass-low.svg'),
+    this.getNewCategory('Alle', 'border-all.svg')];
+
   ngOnInit(): void {
     this.jwtPayload = this.service.decodeJwt(this.service.getJwt());
+  }
+}
+
+export class Category {
+  constructor(
+    public name: string | null,
+    public icon: string | null) {
   }
 }
