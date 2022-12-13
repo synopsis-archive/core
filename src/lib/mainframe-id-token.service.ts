7b2e16a44a25b11ecaf13456b7eb046501f4562f
Klausmd5 <klaus.scheiboeck@vortexdata.net>
2022-12-13 11:54:59 +0100

    udpate nochm gurtner seine feudalen handelsforderungen

--- src/lib/mainframe-id-token.service.ts
+++ src/lib/mainframe-id-token.service.ts
@@ -11,8 +11,7 @@ export class MainframeIdTokenService {
 
   public getJwt(): Promise<string> {
     parent.postMessage({
-      method: "getIDToken",
-      pluginId: "navigation",
+      method: "getIDToken"
     }, "*");
 
     const promise = new Promise<string>((resolve) => {
