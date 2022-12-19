03fefcb5ac8e987e527673e921bec02429eb5a48
TaWi04 <WimmerT180093@sus.htl-grieskirchen.at>
2022-12-19 08:16:53 +0100

    Delete DefaultComponent and Add styles

--- frontend/apps/frontend/src/app/shared/shared.module.ts
+++ frontend/apps/frontend/src/app/shared/shared.module.ts
@@ -2,8 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {EmailInputComponent} from "./email-input/email-input.component";
 import {PasswordInputComponent} from "./password-input/password-input.component";
-import {DefaultInputComponent} from "./default-input/default-input.component";
-import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {FormsModule} from "@angular/forms";
 import {RouterLink} from "@angular/router";
 
 
@@ -11,19 +10,16 @@ import {RouterLink} from "@angular/router";
 @NgModule({
   declarations: [
     EmailInputComponent,
-    PasswordInputComponent,
-    DefaultInputComponent
+    PasswordInputComponent
   ],
   imports: [
     CommonModule,
-    ReactiveFormsModule,
     RouterLink,
     FormsModule
   ],
   exports: [
     EmailInputComponent,
-    PasswordInputComponent,
-    DefaultInputComponent
+    PasswordInputComponent
   ],
 
 })
