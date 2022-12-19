2343b669debeb7dadfb626c963d4ff35d8e210b6
SchoenbergerFabian <schoenbergerf180073@sus.htl-grieskirchen.at>
2022-12-19 12:56:49 +0100

    Appease the CI

--- Core.WebUntis.Interface/Types/Student.cs
+++ Core.WebUntis.Interface/Types/Student.cs
@@ -4,7 +4,7 @@ public class Student
 {
     public int? ClassId { get; set; }
     public int? ClassTeacherId { get; set; }
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
+    public string FirstName { get; set; } = null!;
+    public string LastName { get; set; } = null!;
     public string? Email { get; set; }
 }
