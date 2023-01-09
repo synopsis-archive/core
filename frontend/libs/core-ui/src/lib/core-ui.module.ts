e671490f56736d7fbf3c444e8ed8bb2ea446ac83
littlegamer757 <jneissl27@gmail.com>
2023-01-09 12:47:27 +0100

    Begin syno-button

--- frontend/libs/core-ui/src/lib/core-ui.module.ts
+++ frontend/libs/core-ui/src/lib/core-ui.module.ts
@@ -1,7 +1,12 @@
-import { NgModule } from "@angular/core";
-import { CommonModule } from "@angular/common";
+import {NgModule} from "@angular/core";
+import {CommonModule} from "@angular/common";
+import {SynoButtonComponent} from "./syno-button/syno-button.component";
 
 @NgModule({
   imports: [CommonModule],
+  declarations: [SynoButtonComponent],
+  exports: [
+    SynoButtonComponent
+  ]
 })
 export class CoreUiModule {}
---
+++ frontend/libs/core-ui/src/lib/syno-button/syno-button.component.ts
@@ -0,0 +1,9 @@
+import {Component} from "@angular/core";
+
+@Component({
+  selector: "syno-button",
+  templateUrl: "./syno-button.component.html",
+  styleUrls: ["./syno-button.component.css"],
+})
+export class SynoButtonComponent {
+}
