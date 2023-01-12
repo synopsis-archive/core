e83221e06ef1be77ca593910b53f917bc56b8412
Klausmd5 <klaus.scheiboeck@vortexdata.net>
2023-01-12 13:04:13 +0100

    integrade daily-quote plugin

--- frontend/apps/auth/src/app/login/daily-quote/daily-quote.component.ts
+++ frontend/apps/auth/src/app/login/daily-quote/daily-quote.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from "@angular/core";
+import {Component, OnInit} from "@angular/core";
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: "app-daily-quote",
@@ -6,9 +7,45 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./daily-quote.component.css"],
 })
 export class DailyQuoteComponent implements OnInit {
-  constructor() {}
+  constructor(private http: HttpClient) {
+  }
+
+  configUrl = 'https://localhost:7022/Quotes';
+  Quote: string | null = null;
+  SubmittedBy: string | null = null;
+  QuoteID: string | null = null;
+  QuoteDate: string | null = null;
+  showImage = false;
+
+  getConfig() {
+    return this.http.get<any>(this.configUrl);
+  }
 
   ngOnInit(): void {
-    const seawas = "precommit loss mi commitn du gschissena";
+
+    let currYear = new Date().getFullYear();
+
+    let images = [
+      {start: new Date(currYear, 11, 21), end: new Date(currYear, 11, 26),
+        name: 'XMAS', img: "https://github.com/htl-grieskirchen-core/core-assets/blob/develop/img/lars.jpeg?raw=true", subtxt: "Frohe Weihnachten!"},
+      {start: new Date(currYear, 12, 31), end: new Date(currYear, 0, 2),
+        name: 'NEWYEAR', img: "https://github.com/htl-grieskirchen-core/core-assets/blob/develop/img/f8gqc.gif?raw=true", subtxt: "Frohes neues Jahr!"}
+    ]
+
+    let search = images.find(i => new Date() > i.start && new Date() < i.end);
+    if(search) {
+      this.Quote = search.img;
+      this.SubmittedBy = search.subtxt;
+      this.QuoteID = search.name;
+      this.showImage = true;
+    } else {
+      this.getConfig().subscribe((data) => {
+        this.Quote = data['quoteText'];
+        this.SubmittedBy = data['submittedBy'];
+        this.QuoteID = data['quoteId'];
+        this.QuoteDate = data['submitTime'];
+        console.log(data)
+      });
+    }
   }
 }
--- frontend/apps/auth/src/app/login/login.module.ts
+++ frontend/apps/auth/src/app/login/login.module.ts
@@ -6,10 +6,11 @@ import { DailyQuoteComponent } from "./daily-quote/daily-quote.component";
 import { LoginComponent } from "./login.component";
 import { SharedModule } from "../shared/shared.module";
 import { FormsModule } from "@angular/forms";
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [DailyQuoteComponent, LoginComponent],
-  imports: [CommonModule, SharedModule, LoginRoutingModule, FormsModule],
+  imports: [CommonModule, SharedModule, LoginRoutingModule, FormsModule, HttpClientModule],
   bootstrap: [LoginComponent],
 })
 export class LoginModule {}
