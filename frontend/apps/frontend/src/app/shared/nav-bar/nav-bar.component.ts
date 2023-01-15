25d068dbc066f199bffd89da8c59dd5c6ff1a7c9
Michael Ungar <michael.ungar03@gmail.com>
2023-01-16 00:43:16 +0100

    navbar props

--- frontend/apps/frontend/src/app/shared/nav-bar/nav-bar.component.ts
+++ frontend/apps/frontend/src/app/shared/nav-bar/nav-bar.component.ts
@@ -1,17 +1,29 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, Input, OnInit } from "@angular/core";
+
+interface TabType {
+  id: number;
+  name: string;
+  icon: string;
+}
 
 @Component({
   selector: "app-nav-bar",
   templateUrl: "./nav-bar.component.html",
   styleUrls: ["./nav-bar.component.css"],
 })
-export class NavBarComponent implements OnInit{
-  public val: string = "";
-
+export class NavBarComponent implements OnInit {
   constructor() {}
 
-  ngOnInit(): void {
-    this.val = "nav";
-  }
+  @Input()
+  selectedId: number = 0;
 
+  @Input()
+  tabs: TabType[] = [];
+
+  @Input()
+  onTabClick: (id: number) => void = () => {};
+
+  ngOnInit(): void {
+    this.selectedId = 0;
+  }
 }
