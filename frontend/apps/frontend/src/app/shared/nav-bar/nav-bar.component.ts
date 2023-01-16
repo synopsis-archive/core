1f3c68eadefe456cb969cf987610e84e3c1f3a95
annalena-kagerer <annalena.kagerer@outlook.com>
2023-01-16 08:32:29 +0100

    Add navbar

--- frontend/apps/frontend/src/app/shared/nav-bar/nav-bar.component.ts
+++ frontend/apps/frontend/src/app/shared/nav-bar/nav-bar.component.ts
@@ -6,7 +6,11 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./nav-bar.component.css"],
 })
 export class NavBarComponent implements OnInit{
+  tabs: string[] = ["Home", "Kirtag in Lambrechten", "Netzteil sprengen"];
   public val: string = "";
+  showSearchBar: boolean = false;
+  viewGrid: boolean = true;
+  openTab: string = "Home";
 
   constructor() {}
 
@@ -14,4 +18,24 @@ export class NavBarComponent implements OnInit{
     this.val = "nav";
   }
 
+  searchClicked() {
+    this.showSearchBar = !this.showSearchBar;
+  }
+
+  changeView() {
+    this.viewGrid = !this.viewGrid;
+  }
+
+  showSettings() {
+    // go to settings screen
+  }
+
+  closeTab(tab: string) {
+    this.tabs = this.tabs.filter(x => x !== tab);
+  }
+
+  seas(tab: string) {
+    this.openTab = tab;
+    // show tab
+  }
 }
