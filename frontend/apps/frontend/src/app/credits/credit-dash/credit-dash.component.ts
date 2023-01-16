709109fb98cd717a7ae78ae7104a106e3e84645f
Franz Einboeck <franz.einboeck.htlmail@gmail.com>
2023-01-16 12:34:12 +0100

    make configurable in environment.ts

--- frontend/apps/frontend/src/app/credits/credit-dash/credit-dash.component.ts
+++ frontend/apps/frontend/src/app/credits/credit-dash/credit-dash.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from "@angular/core";
+import { Component } from "@angular/core";
+import {environment} from "../../../environments/environment";
 
 @Component({
   selector: "app-credit-dash",
@@ -6,5 +7,6 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./credit-dash.component.css"],
 })
 export class CreditDashComponent {
+  plugins = environment.plugins;
   constructor() {}
 }
