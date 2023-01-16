3f0af0df3b72da59add79aea59b145981994c130
TaWi04 <wimmert180093@sus.htl-grieskirchen.at>
2023-01-16 08:17:19 +0100

    Add SettingsComponent

--- frontend/apps/frontend/src/app/settings/settings-routing.module.ts
+++ frontend/apps/frontend/src/app/settings/settings-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import {SettingsComponent} from "./settings.component";
+import {SettingsComponent} from "./settings.component";
 
 const routes: Routes = [
   { path: 'settings', component: SettingsComponent }
