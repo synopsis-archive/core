aa48442985cb19fab80561f96d218ee457892ffd
Fabian Gurtner <fabian.paul.gurtner@gmail.com>
2023-01-22 22:41:28 +0100

    Implement plugin routing and upgrade from angular 14 to 15

--- src/lib/plugin-placeholder/plugin-placeholder.component.ts
+++ src/lib/plugin-placeholder/plugin-placeholder.component.ts
@@ -1,18 +1,37 @@
-import {Component, OnInit} from "@angular/core";
-import {ResizedEvent} from "angular-resize-event";
-import {MainframeNavService} from "../mainframe-nav.service";
+import { Component, ElementRef, OnDestroy, OnInit } from "@angular/core";
+import { MainframeNavService } from "../mainframe-nav.service";
+import { SizeMeDim } from "@eisberg-labs/ngx-size-me";
 
 @Component({
   selector: "mainframe-plugin-placeholder",
   templateUrl: "./plugin-placeholder.component.html",
   styleUrls: ["./plugin-placeholder.component.css"]
 })
-export class PluginPlaceholderComponent {
-  constructor(private mainframeNavService: MainframeNavService) {
+export class PluginPlaceholderComponent implements OnInit, OnDestroy {
+  intervalId: number | null = null;
+
+  constructor(private mainframeNavService: MainframeNavService, private elementRef: ElementRef) {
   }
 
-  sendPluginResized(event: ResizedEvent) {
-    let rect = event.newRect;
-    this.mainframeNavService.resizePlugin(rect.left, rect.top, rect.width, rect.height);
+  handleResize(event: SizeMeDim) {
+    this.mainframeNavService.resizePlugin(
+      event.position?.left ?? 0,
+      event.position?.top ?? 0,
+      event.width ?? 0,
+      event.height ?? 0
+    );
+  }
+
+  ngOnInit(): void {
+    this.intervalId = window.setInterval(() => {
+      let rect = this.elementRef.nativeElement.getBoundingClientRect();
+      this.mainframeNavService.resizePlugin(rect.left, rect.top, rect.width, rect.height);
+    }, 100);
+  }
+
+  ngOnDestroy(): void {
+    if (this.intervalId) {
+      window.clearInterval(this.intervalId);
+    }
   }
 }
