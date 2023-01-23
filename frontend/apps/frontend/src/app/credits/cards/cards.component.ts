25cb9fec81112de08839653a86f189f66f8ade72
littlegamer <jneissl27@gmail.com>
2023-01-23 18:06:17 +0100

    Use const instead of let

--- frontend/apps/frontend/src/app/credits/cards/cards.component.ts
+++ frontend/apps/frontend/src/app/credits/cards/cards.component.ts
@@ -57,8 +57,8 @@ export class CardsComponent implements OnInit {
         this.contributors.forEach(x=> {
           this.getCommitCount(x.login).subscribe(count => {
 
-            let frst = this.to2Digits(new Date(count[0].commit.author.date).getMonth()+1).toString();
-            let scnd = new Date(count[0].commit.author.date).getFullYear().toString().substr(2,4);
+            const frst = this.to2Digits(new Date(count[0].commit.author.date).getMonth()+1).toString();
+            const scnd = new Date(count[0].commit.author.date).getFullYear().toString().substr(2,4);
 
             this.lastContribute[x.login] = frst + scnd;
           });
