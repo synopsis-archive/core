8c438ed3da57328ad87153df6f7c784cc64a1a0b
littlegamer <jneissl27@gmail.com>
2023-01-23 17:43:56 +0100

    Move style-switching logic to SynoBaseComponent

--- frontend/libs/core-ui/src/lib/syno-base/syno-base.component.ts
+++ frontend/libs/core-ui/src/lib/syno-base/syno-base.component.ts
@@ -6,6 +6,22 @@ import {Component, Input} from "@angular/core";
   styleUrls: ["./syno-base.component.css"],
 })
 export class SynoBaseComponent {
+  @Input() variant = "";
+  @Input() disabled = false;
   @Input() includeClasses = "";
   @Input() excludeClasses = "";
+
+  setClass(styles: Record<string, string>) {
+    let style = styles["default"];
+
+    this.variant = this.variant.toLowerCase().trim();
+    if (this.variant in styles) style = styles[this.variant];
+    if (this.disabled) style = styles["disabled"];
+
+    style += " " + this.includeClasses;
+    this.excludeClasses.split(" ").forEach(e => style = style.replace(e, ""));
+    style = style.replace(new RegExp(/\s{2,}/), " ");
+
+    return style;
+  }
 }
