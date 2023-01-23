3790492d5718c728d91b5f68ca74c1ae10c967eb
littlegamer757 <jneissl27@gmail.com>
2023-01-23 08:41:18 +0100

    Include libraries in tailwind content paths

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,6 +5,7 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
   content: [
     "./apps/*/src/**/*.{html,ts}",
+    "./libs/*/src/**/*.{html,ts}"
   ],
   theme: {
     extend: {
