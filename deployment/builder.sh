03552cd52f93f7e206efd6cc8a8f2e29643efec7
Klaus Scheiböck <klaus@vortexdata.net>
2023-01-28 00:27:08 +0100

    row => id

--- deployment/builder.sh
+++ deployment/builder.sh
@@ -1,20 +1,20 @@
 conf=$(cat '../mainframe/mainframe-config.json')
 search='https'
 
-for row in $(echo "$conf" | jq -r '.plugins | keys[]'); do
+for id in $(echo "$conf" | jq -r '.plugins | keys[]'); do
 
-    plugin_url=$(echo "$conf" | jq -r ".plugins[\"$row\"] .repoUrl //null")
+    plugin_url=$(echo "$conf" | jq -r ".plugins[\"$id\"] .repoUrl //null")
 
     if grep -q "$search" <<<"$plugin_url"; then
 
-        echo "Downloading $row"
-        curl -L ${plugin_url}"/releases/latest/download/backend.zip" --create-dirs -o "./plugins/backend/$row/backend.zip"
-        unzip -jqo "./plugins/backend/$row/backend.zip" -d "./plugins/backend/$row"
-        rm "./plugins/backend/$row/backend.zip"
+        echo "Downloading $id"
+        curl -L ${plugin_url}"/releases/latest/download/backend.zip" --create-dirs -o "./plugins/backend/$id/backend.zip"
+        unzip -jqo "./plugins/backend/$id/backend.zip" -d "./plugins/backend/$id"
+        rm "./plugins/backend/$id/backend.zip"
 
-        curl -L ${plugin_url}"/releases/latest/download/frontend.zip" --create-dirs -o "./plugins/web/$row/frontend.zip"
-        unzip -jqo "./plugins/web/$row/frontend.zip" -d "./plugins/web/$row"
-        rm "./plugins/web/$row/frontend.zip"
+        curl -L ${plugin_url}"/releases/latest/download/frontend.zip" --create-dirs -o "./plugins/web/$id/frontend.zip"
+        unzip -jqo "./plugins/web/$id/frontend.zip" -d "./plugins/web/$id"
+        rm "./plugins/web/$id/frontend.zip"
     fi
 
-done
+donef
