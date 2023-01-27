bc0dde53fa90d53ffb4028deb25e0a026b1cc050
Klaus Scheiböck <klaus@vortexdata.net>
2023-01-27 23:44:27 +0100

    Update builder.sh

--- deployment/builder.sh
+++ deployment/builder.sh
@@ -1,13 +1,20 @@
-conf=$(curl 'https://raw.githubusercontent.com/htl-grieskirchen-core/core/feature/auto-deploy-update-builder-dings-h%C3%BCfe/mainframe/mainframe-config.json')
+conf=$(cat '../mainframe/mainframe-config.json')
+search='https'
 
 for row in $(echo "$conf" | jq -r '.plugins | keys[]'); do
 
-     plugin_url=$(echo "$conf" | jq -r ".plugins[\"$row\"] .repoUrl")
-     echo $plugin_url
+    plugin_url=$(echo "$conf" | jq -r ".plugins[\"$row\"] .repoUrl //null")
 
+    if grep -q "$search" <<<"$plugin_url"; then
 
-     #echo "$conf" | jq -r '.plugins[] .info .name'
-     curl -o ${row}"/releases/latest/download/backend.zip" "plugins/backend/$row"
-     #echo ${row}"/releases/latest/download/frontend.zip"
+        echo "Downloading $row"
+        curl -L ${plugin_url}"/releases/latest/download/backend.zip" --create-dirs -o "./plugins/backend/$row/backend.zip"
+        unzip -jqo "./plugins/backend/$row/backend.zip" -d "./plugins/backend/$row"
+        rm "./plugins/backend/$row/backend.zip"
+
+        curl -L ${plugin_url}"/releases/latest/download/frontend.zip" --create-dirs -o "./plugins/web/$row/frontend.zip"
+        unzip -jqo "./plugins/web/$row/frontend.zip" -d "./plugins/web/$row"
+        rm "./plugins/web/$row/frontend.zip"
+    fi
 
 done
