1c2555d392b44279eeb004bd0aa0de730bb0e8de
Michael Ungar <michael.ungar03@gmail.com>
2023-01-30 09:44:46 +0100

    .

--- frontend/apps/frontend/src/app/home/plugin/plugin.component.ts
+++ frontend/apps/frontend/src/app/home/plugin/plugin.component.ts
@@ -1,5 +1,5 @@
-import {Component, HostBinding, Input, OnInit} from "@angular/core";
-import {MainframeNavService} from "mainframe-connector";
+import { Component, HostBinding, Input, OnInit } from "@angular/core";
+import { MainframeNavService } from "mainframe-connector";
 
 @Component({
   selector: "app-plugin",
@@ -7,12 +7,11 @@ import {MainframeNavService} from "mainframe-connector";
   styleUrls: ["./plugin.component.scss"],
 })
 export class PluginComponent {
-
   background: string = "";
 
-  @Input() set imageSrc (src: string | null) {
+  @Input() set imageSrc(src: string | null) {
     this.background = `background-image: linear-gradient(#00000000,#000000aa), url(${src})`;
-  };
+  }
   @Input() name: string | null = "";
   @Input() id: string = null!;
 
