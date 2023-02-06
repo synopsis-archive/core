aa25232e05b7475ad44c6027408a85aa66d59885
sgrei <sgreinecker32@yahoo.com>
2023-02-06 10:18:04 +0100

    irwos

--- Core.Database/UserFavorite.cs
+++ Core.Database/UserFavorite.cs
@@ -1,11 +1,5 @@
-using Core.Ldap.Interface;
-using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Core.Database
 {
