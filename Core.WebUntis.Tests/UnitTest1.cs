109e24bf382001719a5f8134a3da30b1824cd7b1
SturmTim <71697727+SturmTim@users.noreply.github.com>
2023-02-13 09:46:42 +0100

    Add BadCredentialException, Create SetUp

--- Core.WebUntis.Tests/UnitTest1.cs
+++ Core.WebUntis.Tests/UnitTest1.cs
@@ -1,15 +1,24 @@
+using System.Text.Json;
+using Core.WebUntis.Implementation;
+using Core.WebUntis.Interface;
+
 namespace Core.WebUntis.Tests;
 
 public class Tests
 {
+    IWebUntisClient _client;
+
     [SetUp]
     public void Setup()
     {
+        var username = Environment.GetEnvironmentVariable("WEBUNTIS_USERNAME");
+        var password = Environment.GetEnvironmentVariable("WEBUNTIS_PASSWORD");
+        _client = new WebUntisClient("https://arche.webuntis.com", "htbla-grieskirchen", "Synopsis-Test");
+        _client.Authenticate(username, password);
     }
 
     [Test]
-    public void Test1()
+    public void TestRooms()
     {
-        Assert.Pass();
     }
 }
