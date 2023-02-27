b86290a23b59873020cd1860800401ce3d5af68d
SturmTim <71697727+SturmTim@users.noreply.github.com>
2023-02-27 08:26:23 +0100

    Add await to Authenticate, Add Test for each Method in Client

--- Core.WebUntis.Tests/UnitTest1.cs
+++ Core.WebUntis.Tests/UnitTest1.cs
@@ -1,26 +1,57 @@
 using System.Text.Json;
 using Core.WebUntis.Implementation;
 using Core.WebUntis.Interface;
+using Core.WebUntis.Interface.Types;
 
 namespace Core.WebUntis.Tests;
 
 public class Tests
 {
-    IWebUntisClient _client;
+    WebUntisClient _client;
 
     [SetUp]
-    public void Setup()
+    public async Task Setup()
     {
         var username = Environment.GetEnvironmentVariable("WEBUNTIS_USERNAME");
         var password = Environment.GetEnvironmentVariable("WEBUNTIS_PASSWORD");
         _client = new WebUntisClient("https://arche.webuntis.com", "htbla-grieskirchen", "Synopsis-Test");
-        _client.Authenticate(username, password);
+        await _client.Authenticate(username, password);
     }
 
     [Test]
     public async Task TestRooms()
     {
         await TestMethod(async () => { await _client.GetRooms(); });
+    }
+
+    [Test]
+    public async Task TestClasses()
+    {
+        await TestMethod(async () => { await _client.GetClasses(); });
+    }
+
+    [Test]
+    public async Task TestHolidays()
+    {
+        await TestMethod(async () => { await _client.GetHolidays(); });
+    }
+
+    [Test]
+    public async Task TestHomeworks()
+    {
+        await TestMethod(async () => { await _client.GetHomeworks(DateTime.Now, DateTime.Now.AddDays(1)); });
+    }
+
+    [Test]
+    public async Task TestSubject()
+    {
+        await TestMethod(async () => { await _client.GetSubjects(); });
+    }
+
+    [Test]
+    public async Task TestTimetable()
+    {
+        await TestMethod(async () => { await _client.GetTimetable(ElementType.Student, _client.PersonId, DateTime.Now, DateTime.Now.AddDays(1)); });
     }
 
     private static async Task TestMethod(Func<Task> function)
