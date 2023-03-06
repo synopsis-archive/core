b076c8e1dbf495fd0c7745bb91925b5d8a81d68f
Lea <seifried.lea@gmail.com>
2023-03-06 10:22:41 +0100

    Update search view

--- frontend/apps/frontend/src/app/shared/search/search.component.ts
+++ frontend/apps/frontend/src/app/shared/search/search.component.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+import {SearchService} from "../../core/search.service";
 
 @Component({
   selector: "app-search",
@@ -6,10 +7,14 @@ import { Component } from "@angular/core";
   styleUrls: ["./search.component.css"],
 })
 export class SearchComponent {
-  constructor() {}
+  constructor(private searchService: SearchService) {}
   public searchterm: string = "";
 
   search() {
     console.log(this.searchterm);
+  }
+
+  close() {
+    this.searchService.isSearchShown.next(false);
   }
 }
