0cbf5078afa86fe295d2cc750067e4d1285ca2bb
TaWi04 <WimmerT180093@sus.htl-grieskirchen.at>
2023-03-06 08:25:03 +0100

    Fix layout Fix accordion

--- frontend/apps/frontend/src/app/settings/settings.module.ts
+++ frontend/apps/frontend/src/app/settings/settings.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
-
 import { SettingsRoutingModule } from "./settings-routing.module";
 import { SettingsComponent } from "./settings.component";
 import {FormsModule} from "@angular/forms";
+import {CoreUiModule} from "core-ui";
 
 @NgModule({
   declarations: [SettingsComponent],
   imports: [CommonModule, SettingsRoutingModule,
-    FormsModule],
+    FormsModule, CoreUiModule],
   exports: [SettingsComponent]
 })
 export class SettingsModule {}
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,7 +5,8 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
   content: [
     "./apps/*/src/**/*.{html,ts}",
-    "./libs/*/src/**/*.{html,ts}"
+    "./libs/*/src/**/*.{html,ts}",
+    "./node_modules/flowbite/**/*.js"
   ],
   theme: {
     extend: {
@@ -17,5 +18,7 @@ module.exports = {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin')
+  ],
 }
