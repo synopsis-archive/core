71009f07149cb1d5324b2b2152741c65620c26c3
littlegamer757 <jneissl27@gmail.com>
2023-03-13 10:00:17 +0100

    Update handling multiple children in syno-button

--- frontend/libs/core-ui/src/lib/syno-button/syno-button.component.ts
+++ frontend/libs/core-ui/src/lib/syno-button/syno-button.component.ts
@@ -7,12 +7,9 @@ import {SynoBaseComponent} from "../syno-base/syno-base.component";
   styleUrls: ["./syno-button.component.css"],
 })
 export class SynoButtonComponent extends SynoBaseComponent {
+
   styles = {
     "default": "bg-gradient-to-r from-synoblue to-synogreen h-10 rounded-lg p-[1.5px] pl-[1.6px] box-border flex justify-center items-center",
-    // "disabled": "py-2.5 px-5 mr-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-full border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700",
-    // "green": "focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-full text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800",
-    // "red": "focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-full text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900",
-    // "yellow": "focus:outline-none text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-full text-sm px-5 py-2.5 mr-2 mb-2 dark:focus:ring-yellow-900",
-    // "purple": "focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-full text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900",
   };
+
 }
