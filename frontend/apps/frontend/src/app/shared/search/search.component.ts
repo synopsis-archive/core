725df9768057b4923054a85041882205b5e57f69
annalena-kagerer <annalena.kagerer@outlook.com>
2023-03-20 08:44:16 +0100

    Add logic for tag-search

--- frontend/apps/frontend/src/app/shared/search/search.component.ts
+++ frontend/apps/frontend/src/app/shared/search/search.component.ts
@@ -14,9 +14,19 @@ export class SearchComponent implements OnInit {
   }
 
   public searchterm: string = "";
-  public results: Plugin[] = [];
 
+  public results: Plugin[] = [];
   private plugins: Plugin[] = [];
+  private tags: string[] = [];
+  private selectedTags: string[] = [];
+
+  ngOnInit(): void {
+    this.pluginService.getPluginList().then((plugins: Plugin[]) => {
+      this.plugins = plugins.sort((a, b) => a.name.localeCompare(b.name));
+      this.tags = [...new Set(this.plugins.flatMap((x) => x.tags))];
+      this.results = this.plugins;
+    });
+  }
 
   search() {
     this.searchTitle();
@@ -24,21 +34,14 @@ export class SearchComponent implements OnInit {
   }
 
   searchTitle() {
-    this.results = this.plugins.filter((x) => x.name.toUpperCase().includes(this.searchterm.toUpperCase()));
+    this.results = this.plugins.filter(x => x.name.toUpperCase().includes(this.searchterm.toUpperCase()));
   }
 
   searchTags() {
-
+    this.plugins.filter(x => this.selectedTags.forEach(t => x.tags.includes(t)));
   }
 
   close() {
     this.searchService.isSearchShown.next(false);
-  }
-
-  ngOnInit(): void {
-    this.pluginService.getPluginList().then((plugins: Plugin[]) => {
-      this.plugins = plugins.sort((a, b) => a.name.localeCompare(b.name));
-      this.results = this.plugins;
-    });
   }
 }
