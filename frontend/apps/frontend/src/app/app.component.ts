bab018f2890caf561eb326b62c6fade7008bd5ff
littlegamer757 <jneissl27@gmail.com>
2023-03-27 09:30:41 +0200

    Move tagColors back to main project

--- frontend/apps/frontend/src/app/app.component.ts
+++ frontend/apps/frontend/src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from "@angular/core";
-import { SearchService } from "./core/search.service";
-import { NavBarService } from "./core/nav-bar.service";
+import {Component, OnInit} from "@angular/core";
+import {SearchService} from "./core/search.service";
+import {Plugin, PluginListService} from "mainframe-connector";
+import {setTagColors} from "./shared/tagColors";
 
 @Component({
   selector: "app-root",
@@ -11,10 +12,15 @@ export class AppComponent implements OnInit {
   title = "frontend";
   isSearchShown: boolean = false;
 
-  constructor(public searchService: SearchService) {}
+  constructor(public searchService: SearchService, private pluginService: PluginListService) {}
   ngOnInit(): void {
     this.searchService.isSearchShown.subscribe((x) => {
       this.isSearchShown = x;
+    });
+
+    this.pluginService.getPluginList().then((plugins: Plugin[]) => {
+      const tags = [...new Set(plugins.flatMap(x => x.tags))];
+      setTagColors(tags);
     });
   }
 }
