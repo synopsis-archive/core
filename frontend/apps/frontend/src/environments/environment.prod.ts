52511ee438c41d3187bcbc906d1e39d443e397c9
Lea <seifried.lea@gmail.com>
2023-04-17 09:17:06 +0200

    Add backendURL

--- frontend/apps/frontend/src/environments/environment.prod.ts
+++ frontend/apps/frontend/src/environments/environment.prod.ts
@@ -5,4 +5,5 @@ export const environment = {
     { repoOwner: "htl-grieskirchen-core", name: "Polls", repoName: "plugin-polls" },
     { repoOwner: "htl-grieskirchen-core", name: "Template", repoName: "plugin-template" },
   ],
+  backendURL: "http://localhost:5256"
 };
