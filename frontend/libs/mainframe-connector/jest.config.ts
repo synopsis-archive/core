eee10e0cf0af5df5bfafec1ad21313a746967e53
Fabian <fabian.paul.gurtner@gmail.com>
2023-04-17 10:06:32 +0200

    Update nx

--- frontend/libs/mainframe-connector/jest.config.ts
+++ frontend/libs/mainframe-connector/jest.config.ts
@@ -1,22 +1,23 @@
 /* eslint-disable */
 export default {
-  displayName: 'mainframe-connector',
-  preset: '../../jest.preset.js',
-  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
-  globals: {
-    'ts-jest': {
-      tsconfig: '<rootDir>/tsconfig.spec.json',
-      stringifyContentPathRegex: '\\.(html|svg)$',
-    },
-  },
-  coverageDirectory: '../../coverage/libs/mainframe-connector',
+  displayName: "mainframe-connector",
+  preset: "../../jest.preset.js",
+  setupFilesAfterEnv: ["<rootDir>/src/test-setup.ts"],
+  globals: {},
+  coverageDirectory: "../../coverage/libs/mainframe-connector",
   transform: {
-    '^.+\\.(ts|mjs|js|html)$': 'jest-preset-angular',
+    "^.+\\.(ts|mjs|js|html)$": [
+      "jest-preset-angular",
+      {
+        tsconfig: "<rootDir>/tsconfig.spec.json",
+        stringifyContentPathRegex: "\\.(html|svg)$",
+      },
+    ],
   },
-  transformIgnorePatterns: ['node_modules/(?!.*\\.mjs$)'],
+  transformIgnorePatterns: ["node_modules/(?!.*\\.mjs$)"],
   snapshotSerializers: [
-    'jest-preset-angular/build/serializers/no-ng-attributes',
-    'jest-preset-angular/build/serializers/ng-snapshot',
-    'jest-preset-angular/build/serializers/html-comment',
+    "jest-preset-angular/build/serializers/no-ng-attributes",
+    "jest-preset-angular/build/serializers/ng-snapshot",
+    "jest-preset-angular/build/serializers/html-comment",
   ],
 };
